dfd39ecdec42fea366ab502fe0c8c1ce451947df
Pablo Gallina <pablogallinag@gmail.com>
2021-04-16 15:38:21 +0200

    Usando Templates html

--- proyecto_No1/aplicacion1/urls.py
+++ proyecto_No1/aplicacion1/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.aplicacion1, name='aplicacion1')
+    path('', views.aplicacion1, name='aplicacion1'),
+    path('<str:nombre>/', views.saludar, name='saludo'),
 ]
--- proyecto_No1/aplicacion1/views.py
+++ proyecto_No1/aplicacion1/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def aplicacion1(request):
-    return HttpResponse("Hola mundo")
+    #return HttpResponse("Hola mundo")
+    return render(request, 'aplicacion1/index.html')
+
+def saludar(request, nombre):
+    return HttpResponse(f"Hola {nombre}")
+
+    
