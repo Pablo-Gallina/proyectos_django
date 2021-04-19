117d48c3a37a4f6fdd768029a5faadb5b1bb6f94
Pablo Gallina <pablogallinag@gmail.com>
2021-04-19 08:43:28 +0200

    Formulario de agregar

--- Proyecto_No2/listado/views.py
+++ Proyecto_No2/listado/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import AgregarTarea
 
 tareas = ['Limpiar', 'Investigar', 'Estudiar']
 # Create your views here.
@@ -6,4 +7,12 @@ def home(request):
     context = {'tareas': tareas}
     return render(request, 'lista/index.html', context)
 def add(request):
-    return render(request, 'lista/add.html')
+    if request.method == 'POST':
+        form = AgregarTarea(request.POST)
+        if form.is_valid():
+            tarea = form.cleaned_data['tarea']
+            tareas.append(tarea)
+    else:
+        form = AgregarTarea()
+    context = {'form': form}
+    return render(request, 'lista/add.html', context)
