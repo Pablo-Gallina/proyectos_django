5c71266ade9eff19660d0c65352152df621526fd
Pablo Gallina <pablogallinag@gmail.com>
2021-04-19 10:14:28 +0200

    Model creado y agregado al sitio administrativo

--- Proyecto_No3/pruebadb/models.py
+++ Proyecto_No3/pruebadb/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Empresa(models.Model):
+    # Campos
+    nombre = models.CharField(max_length=20, help_text="Enter field documentation")
+    fundacion = models.IntegerField()
