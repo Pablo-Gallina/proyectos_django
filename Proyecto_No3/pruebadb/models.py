2b4a11fd71b5d45179dafa22e386ec7e65fce5f5
Pablo Gallina <pablogallinag@gmail.com>
2021-04-19 11:24:45 +0200

    Datos agregados a la bd

--- Proyecto_No3/pruebadb/models.py
+++ Proyecto_No3/pruebadb/models.py
@@ -4,3 +4,24 @@ class Empresa(models.Model):
     # Campos
     nombre = models.CharField(max_length=20, help_text="Enter field documentation")
     fundacion = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+
+class Lenguaje(models.Model):
+    nombre = models.CharField(max_length=40)
+    creador = models.CharField(max_length=40, null=True)
+
+    def __str__(self):
+        return self.nombre
+    
+class Programador(models.Model):
+    nombre = models.CharField(max_length=20, help_text="Enter field documentation")
+    edad = models.IntegerField(null=True)
+    empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE,  related_name="empleados") #SET_NULL para que cuando se borre la fk, el registro siga existiendo
+    lenguaje = models.ManyToManyField(Lenguaje)
+    
+    def __str__(self):
+        return self.nombre
+    
+        
